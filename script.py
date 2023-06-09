1751911d84c2d0c4aeb8336a9e34854fa79382d7
AGSHENDRY <115783148+AGSHENDRY@users.noreply.github.com>
2023-06-10 00:46:21 +0530

    Update script.py

--- script.py
+++ script.py
@@ -34,13 +34,13 @@ Add me as admin in your group and start filtering :)
 /id  -  Shows ID information
 /info userid  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
-© @Mo_Tech_YT
+© @movies_hub
 """
 
 AboutTxT = """
-⭕️ My Name : TroJanz Filter Bot</b>
-⭕️ Developers : @TroJanzHEX 
-⭕️ Upgrader : @Mo_Tech_YT
+⭕️ My Name : Ags Filter Bot</b>
+⭕️ Developers : @movies_hub
+⭕️ Upgrader : @alaxander_bot_Ak
 ⭕️ Language : Python3
 ⭕️ Library : <a href='https://docs.pyrogram.org/'>Pyrogram 2.0.102</a> 
 """
